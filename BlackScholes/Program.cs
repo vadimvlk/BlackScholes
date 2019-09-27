ffaf1574123d0ce9293a0734ae6409333ce4f0ca
SrvDev Volkov <SrvDev.Volkov@S5000VSS003>
2019-09-27 15:45:55 +0300

    initial add

---
+++ BlackScholes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlackScholes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
