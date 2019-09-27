fdbad468e3105731728db1f2925cd2bf3efbcf8f
SrvDev Volkov <SrvDev.Volkov@S5000VSS003>
2019-09-27 15:49:48 +0300

    add Interface IBlackScholes

---
+++ BlackScholes/IBlackScholes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackScholes
+{
+
+    interface IBlackScholes
+    {
+        double d_1(double S, double X, double d, int y, double v);
+        double d_2(double S, double X, double d, int y, double v);
+        double Nd_1(double S, double X, double d, int y, double v);
+        double Nd_2(double S, double X, double d, int y, double v);
+        double N_d_1(double S, double X, double d, int y, double v);
+        double N_d_2(double S, double X, double d, int y, double v);
+        double N1d_1(double S, double X, double d, int y, double v);
+        double OptionPrice(Char optionType, double S, double X, double d, int y, double v);
+        double Delta(Char optionType, double S, double X, double d, int y, double v);
+        double Theta(double S, double X, double d, int y, double v);
+        double Gamma(double S, double X, double d, int y, double v);
+        double Vega(double S, double X, double d, int y, double v);
+        double NormSDist(double num);
+    }
+
+}
