f1678e87e57678948b4980e8e46ee8aa546f053c
SrvDev Volkov <SrvDev.Volkov@S5000VSS003>
2019-09-27 15:54:11 +0300

    add Main Class BlackSchoulZZZ

--- BlackScholes/Program.cs
+++ BlackScholes/Program.cs
@@ -4,9 +4,40 @@ namespace BlackScholes
 {
     class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            Console.WriteLine("Hello World!");
+
+            public static void Main(string[] args)
+            {
+                A:
+                Console.WriteLine("Input current Price: ");
+                double S = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Input Strike: ");
+                double X = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Input day for expiry: ");
+                double d = Convert.ToDouble(Console.ReadLine());
+                int y = 365;
+                Console.WriteLine("Input IV: ");
+                double v = Convert.ToDouble(Console.ReadLine());
+
+
+                BlackSchoulZZZ blackShould = new BlackSchoulZZZ();
+
+
+                Console.WriteLine("Премия Call : " + (blackShould.OptionPrice('C', S, X, d, y, v)) / S);
+                Console.WriteLine("Премия Put : " + (blackShould.OptionPrice('P', S, X, d, y, v)) / S);
+                Console.WriteLine("Delta Call : " + blackShould.Delta('C', S, X, d, y, v));
+                Console.WriteLine("Delta Put" + blackShould.Delta('P', S, X, d, y, v));
+                Console.WriteLine("Gamma  : " + blackShould.Gamma(S, X, d, y, v));
+                Console.WriteLine("Vega :" + blackShould.Vega(S, X, d, y, v));
+                Console.WriteLine("Theta :" + blackShould.Theta(S, X, d, y, v));
+
+
+                Console.ReadLine();
+
+                goto A;
+
+            }
         }
     }
 }
